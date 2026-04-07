54e12a747a472de3614c7c467398563b454eb600
nguyekim99-sudo <nguyekim99@gmail.com>
2026-04-08 03:25:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+﻿const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`Bot đã online: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === 'hello') {
+    message.reply('Chào bạn 👋');
+  }
+});
+
+client.login('');
